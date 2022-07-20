18eb83788da8619c6a0ce848852aa23d812f648c
Ghost051 <100504833+Ghost051@users.noreply.github.com>
2022-07-20 15:39:29 -0400

    Add files via upload

---
+++ new_acc.js
@@ -0,0 +1,8 @@
+function password2() {
+    var x = document.getElementById("password2");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
